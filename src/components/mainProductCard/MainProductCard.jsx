7321de89a24f6f436597606cdd271da067415ff3
Ali Golizadeh <loooper06@gmail.com>
2022-05-28 15:34:12 +0430

    as

--- src/components/mainProductCard/MainProductCard.jsx
+++ src/components/mainProductCard/MainProductCard.jsx
@@ -146,202 +146,237 @@ const MainProductCard = (props) => {
   });
 
   return (
-    <Container fluid>
-      <Row>
-        <Col xs={12} md={12} lg={7} className="p-5">
-          <div className="mainProductImg">
-            <img src={mainImage} alt={title} />
-          </div>
-          <div className=" mainSubImg">
-            {mainImages.map((img) => (
-              <div
-                key={img.id}
-                className={`SubImg ${img.active ? "Bordered" : ""}`}
-                onClick={() => activeImg(img.id)}
-              >
-                <img src={img.img} alt="productSub" />
-              </div>
-            ))}
-          </div>
-        </Col>
-        <Col xs={12} md={12} lg={5} className="py-5 px-3">
-          <div className=" mainContentWrapper">
-            <div className=" mainProductTitle">
-              <div>
-                <h3>{title}</h3>
-              </div>
-              <div>
-                <span className={inStock === 0 ? "outOfStock" : ""}>
-                  {inStock > 0 ? "" : "out of Stock"}
-                </span>
-                {discount ? (
-                  <span
-                    className={`mx-2 ${discount === 0 ? "" : "discountTag"}`}
-                  >
-                    {discount === 0 ? "" : `${discount} % Off`}
+    <>
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={12} lg={7} className="p-5">
+            <div className="mainProductImg">
+              <img src={mainImage} alt={title} />
+            </div>
+            <div className=" mainSubImg">
+              {mainImages.map((img) => (
+                <div
+                  key={img.id}
+                  className={`SubImg ${img.active ? "Bordered" : ""}`}
+                  onClick={() => activeImg(img.id)}
+                >
+                  <img src={img.img} alt="productSub" />
+                </div>
+              ))}
+            </div>
+          </Col>
+          <Col xs={12} md={12} lg={5} className="py-5 px-3">
+            <div className=" mainContentWrapper">
+              <div className=" mainProductTitle">
+                <div>
+                  <h3>{title}</h3>
+                </div>
+                <div>
+                  <span className={inStock === 0 ? "outOfStock" : ""}>
+                    {inStock > 0 ? "" : "out of Stock"}
                   </span>
+                  {discount ? (
+                    <span
+                      className={`mx-2 ${discount === 0 ? "" : "discountTag"}`}
+                    >
+                      {discount === 0 ? "" : `${discount} % Off`}
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+              <div className="mainProductPrice">
+                <h4 className={discount > 0 ? "corePrice" : ""}>
+                  {discount > 0 ? price : price + " $"}
+                </h4>
+                {discount > 0 ? (
+                  <h4 className="dicountedPrice">{priceAfterDiscount} $</h4>
                 ) : (
                   <></>
                 )}
               </div>
-            </div>
-            <div className="mainProductPrice">
-              <h4 className={discount > 0 ? "corePrice" : ""}>
-                {discount > 0 ? price : price + " $"}
-              </h4>
-              {discount > 0 ? (
-                <h4 className="dicountedPrice">{priceAfterDiscount} $</h4>
-              ) : (
-                <></>
-              )}
-            </div>
-            <div className="mainProductDetail">
-              <article className="detailItem">
-                <h5 className="detailHead">category : </h5>
-                <p className="detailContent">{categoryGender}</p>
-              </article>
-              <article className="detailItem">
-                <h5 className="detailHead">In Stock : </h5>
-                <p className="detailContent">{inStock} item</p>
-              </article>
-              <article className="detailItem">
-                <h5 className="detailHead">Discount : </h5>
-                <p className="detailContent">{discount} %</p>
-              </article>
-            </div>
-            <div className="mainProductDescription">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                distinctio laudantium ut fugiat nisi voluptatibus ratione
-                perspiciatis iure molestiae eius quidem quisquam totam quasi,
-                eos est optio quaerat laborum eum repellat, hic nam velit
-                necessitatibus impedit. Repellendus libero error esse voluptate.
-                Est fuga optio asperiores!
-              </p>
-            </div>
-            <div className=" mainProductColorPick">
-              <p className="colorPickHead">Color : </p>
-              <ul className="colorPickList">
-                {mainProductColor.map((color) => (
-                  <li
-                    key={color.id}
-                    className={color.active ? "Bordered" : ""}
-                    onClick={() => activeColor(color.id)}
-                  >
-                    <div className={`${color.color} colorPickListItem`}></div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className=" mainProductSize">
-              <p className="SizeHead">Size : </p>
-              <ul className="SizeList">
-                {mainProductSize.map((size) => (
-                  <li
-                    key={size.id}
-                    className={size.active ? "Bordered" : ""}
-                    onClick={() => activeSize(size.id)}
-                  >
-                    {size.size}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <Container className="mainProductAddtoCart">
-              <Row>
-                <Col xs={12} sm={4} xl={4}>
-                  <div className=" mainProductAmount">
-                    <ButtonGroup
-                      className="w-100"
-                      color="secondary"
-                      size="large"
-                      aria-label="medium secondary button group"
+              <div className="mainProductDetail">
+                <article className="detailItem">
+                  <h5 className="detailHead">category : </h5>
+                  <p className="detailContent">{categoryGender}</p>
+                </article>
+                <article className="detailItem">
+                  <h5 className="detailHead">In Stock : </h5>
+                  <p className="detailContent">{inStock} item</p>
+                </article>
+                <article className="detailItem">
+                  <h5 className="detailHead">Discount : </h5>
+                  <p className="detailContent">{discount} %</p>
+                </article>
+              </div>
+              <div className="mainProductDescription">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima distinctio laudantium ut fugiat nisi voluptatibus
+                  ratione perspiciatis iure molestiae eius quidem quisquam totam
+                  quasi, eos est optio quaerat laborum eum repellat, hic nam
+                  velit necessitatibus impedit. Repellendus libero error esse
+                  voluptate. Est fuga optio asperiores!
+                </p>
+              </div>
+              <div className=" mainProductColorPick">
+                <p className="colorPickHead">Color : </p>
+                <ul className="colorPickList">
+                  {mainProductColor.map((color) => (
+                    <li
+                      key={color.id}
+                      className={color.active ? "Bordered" : ""}
+                      onClick={() => activeColor(color.id)}
                     >
-                      {amountButtons}
-                    </ButtonGroup>
-                  </div>
-                </Col>
-                <Col xs={12} sm={8} xl={8}>
-                  <div className=" mainProductSubmit">
-                    <button
-                      onClick={() => addToCart(id)}
-                      className="border py-3 w-100 py-2 addToCartSubmitButton"
-                      disabled={inStock == 0}
+                      <div className={`${color.color} colorPickListItem`}></div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className=" mainProductSize">
+                <p className="SizeHead">Size : </p>
+                <ul className="SizeList">
+                  {mainProductSize.map((size) => (
+                    <li
+                      key={size.id}
+                      className={size.active ? "Bordered" : ""}
+                      onClick={() => activeSize(size.id)}
                     >
-                      Add to Cart
-                    </button>
-                  </div>
-                </Col>
-                <Col xs={12} md={12} xl={6}>
-                  <div className=" mainProductAddWishlist">
-                    <button className="addProductToWishlist">
-                      <FavoriteBorderOutlinedIcon />
-                      Add to Favorites
-                    </button>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-            <div className="mainProductCates">
-              <div className="cate categoryCate">
-                <EditOutlinedIcon />
-                <h5 className="me-4 ms-2">Category : </h5>
-                <Breadcrumbs aria-label="breadcrumb">
-                  <Link to="/">Home</Link>
-                  <Link
-                    to={
-                      categoryGender === "Men" || categoryGender === "Women"
-                        ? `/shop/clothes/${categoryGender.toLowerCase()}`
-                        : `/shop/${categoryGender.toLowerCase()}`
-                    }
-                  >
-                    {categoryGender}
-                  </Link>
-                </Breadcrumbs>
+                      {size.size}
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <div className="cate tagsCate">
-                <BeenhereOutlinedIcon />
-                <h5 className="me-4 ms-2">Tags : </h5>
-                <Breadcrumbs aria-label="breadcrumb" separator=",">
-                  <Typography color="text.primary">mamd</Typography>
-                  <Typography color="text.primary">ali</Typography>
-                  <Typography color="text.primary">amir</Typography>
-                </Breadcrumbs>
+              <Container className="mainProductAddtoCart">
+                <Row>
+                  <Col xs={12} sm={4} xl={4}>
+                    <div className=" mainProductAmount">
+                      <ButtonGroup
+                        className="w-100"
+                        color="secondary"
+                        size="large"
+                        aria-label="medium secondary button group"
+                      >
+                        {amountButtons}
+                      </ButtonGroup>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={8} xl={8}>
+                    <div className=" mainProductSubmit">
+                      <button
+                        onClick={() => addToCart(id)}
+                        className="border py-3 w-100 py-2 addToCartSubmitButton"
+                        disabled={inStock == 0}
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  </Col>
+                  <Col xs={12} md={12} xl={6}>
+                    <div className=" mainProductAddWishlist">
+                      <button className="addProductToWishlist">
+                        <FavoriteBorderOutlinedIcon />
+                        Add to Favorites
+                      </button>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+              <div className="mainProductCates">
+                <div className="cate categoryCate">
+                  <EditOutlinedIcon />
+                  <h5 className="me-4 ms-2">Category : </h5>
+                  <Breadcrumbs aria-label="breadcrumb">
+                    <Link to="/">Home</Link>
+                    <Link
+                      to={
+                        categoryGender === "Men" || categoryGender === "Women"
+                          ? `/shop/clothes/${categoryGender.toLowerCase()}`
+                          : `/shop/${categoryGender.toLowerCase()}`
+                      }
+                    >
+                      {categoryGender}
+                    </Link>
+                  </Breadcrumbs>
+                </div>
+                <div className="cate tagsCate">
+                  <BeenhereOutlinedIcon />
+                  <h5 className="me-4 ms-2">Tags : </h5>
+                  <Breadcrumbs aria-label="breadcrumb" separator=",">
+                    <Typography color="text.primary">mamd</Typography>
+                    <Typography color="text.primary">ali</Typography>
+                    <Typography color="text.primary">amir</Typography>
+                  </Breadcrumbs>
+                </div>
+              </div>
+              <div className="mainProductSubDetail">
+                <div className="mainProductSubDetailItem">
+                  <div className="subDetailItemImg">
+                    <img
+                      src="/image/securityProductDetail.jpg"
+                      alt="Security"
+                    />
+                  </div>
+                  <div className="subDetailItemContext">
+                    <article>Security policy</article>
+                    <article>
+                      (edit with the Customer Reassurance module)
+                    </article>
+                  </div>
+                </div>
+                <div className="mainProductSubDetailItem">
+                  <div className="subDetailItemImg">
+                    <img
+                      src="/image/deliveryProductDetail_1.jpg"
+                      alt="Delivery"
+                    />
+                  </div>
+                  <div className="subDetailItemContext">
+                    <article>Delivery policy</article>
+                    <article>
+                      (edit with the Customer Reassurance module)
+                    </article>
+                  </div>
+                </div>
+                <div className="mainProductSubDetailItem">
+                  <div className="subDetailItemImg">
+                    <img src="/image/policyProductDetail.jpg" alt="Policy" />
+                  </div>
+                  <div className="subDetailItemContext">
+                    <article>Return policy</article>
+                    <article>
+                      (edit with the Customer Reassurance module)
+                    </article>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="mainProductSubDetail">
-              <div className="mainProductSubDetailItem">
-                <div className="subDetailItemImg">
-                    <img src="/image/securityProductDetail.jpg" alt="Security" />
-                </div>
-                <div className="subDetailItemContext">
-                  <article>Security policy</article>
-                  <article>(edit with the Customer Reassurance module)</article>
-                </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={4} className='ps-5'>
+            <div className="mainProductOptionsTitles">
+              <div className="mainProductOptionTitle">
+                <h5>Description</h5>
               </div>
-              <div className="mainProductSubDetailItem">
-                <div className="subDetailItemImg">
-                  <img src="/image/deliveryProductDetail_1.jpg" alt="Delivery" />
-                </div>
-                <div className="subDetailItemContext">
-                  <article>Delivery policy</article>
-                  <article>(edit with the Customer Reassurance module)</article>
-                </div>
+              <div className="mainProductOptionTitle">
+                <h5>Product Details</h5>
               </div>
-              <div className="mainProductSubDetailItem">
-                <div className="subDetailItemImg">
-                  <img src="/image/policyProductDetail.jpg" alt="Policy" />
-                </div>
-                <div className="subDetailItemContext">
-                  <article>Return policy</article>
-                  <article>(edit with the Customer Reassurance module)</article>
-                </div>
+              <div className="mainProductOptionTitle">
+                <h5>Comment's</h5>
+              </div>
+              <div className="mainProductOptionTitle">
+                <h5>Reviews</h5>
               </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+          <Col xs={12} md={8} className='pe-5'></Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
